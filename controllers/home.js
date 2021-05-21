de3cf67cfbfbbe6523515bd4d48e18cfb7a4834f
Your Name <cauanlima5@gmail.com>
2021-05-21 09:52:37 -0300

    fix: adicionei cors para que houvesse comunicaçao

--- controllers/home.js
+++ controllers/home.js
@@ -1,10 +1,13 @@
 const bancoDeDados = [];
+
 function listarTodos(req, res) {
   return res.json(bancoDeDados);
 }
 function adicionar(req, res) {
+  console.log(req.body)
   if (req.body !== "") {
     bancoDeDados.push(req.body);
+    console.log(req.body);
     return res.json({ mensagem: "cadastrado com sucesso" });
   } else {
     res.json({ mensagem: "Não é possivel cadastro" });
--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+var cors = require('cors')
 require("dotenv").config();
+
 
 const routes = require("./routes/routes");
 
 const app = express();
+app.use(cors())
 
-app.use(bodyParser.json());
+app.use(express.json())
+
+// app.use(bodyParser.json());
 
 app.use(routes);
 
-app.listen(3000, function () {
+app.listen(3335, function () {
   console.log("Listening");
 });
